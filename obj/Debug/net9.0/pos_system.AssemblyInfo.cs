cdcf997cff01572f0a294da77a7c4e70c51241e3
Edzra Macas <EdzraM7@gmail.com>
2024-12-18 13:45:50 +0800

    Ayaw huhuhu

--- obj/Debug/net9.0/pos_system.AssemblyInfo.cs
+++ obj/Debug/net9.0/pos_system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9514d6caae7e38a42c66b5438315414224b363a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c8eb5bba393999275f251133c6750daa4ccee3")]
 [assembly: System.Reflection.AssemblyProductAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
